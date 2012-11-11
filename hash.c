fb0275c46860df5378cd84ac9df0b4ad95be67c8
coolSCV <josh.h.andersen@gmail.com>
2012-11-10 20:56:17 -0800

    removed jenkinshash

--- hash.c
+++ hash.c
@@ -1,5 +1,15 @@
 #include "loglog.h"
 
+uint32_t hammingDistance(uint32_t x) {
+  uint32_t dist = 0;
+  uint32_t val =   x ^ 0xFFFFFFFF;
+  while(val) {
+    ++dist; 
+    val &= val - 1;
+  }
+  return dist;
+}
+
 uint32_t leadingZeroCount(uint32_t x) {
   x |= (x >> 1);
   x |= (x >> 2);
@@ -16,31 +26,4 @@ uint32_t ones(uint32_t x) {
   x += (x >> 8);
   x += (x >> 16);
   return(x & 0x0000003F);
-}
-
-uint32_t jenkinsHash(const void *data, size_t len) {
-  char * key = (char *)data;
-  uint32_t hash, i;
-  for(hash = i = 0; i < len; ++i) {
-    hash += key[i];
-    hash += (hash << 10);
-    hash ^= (hash >> 6);
-  }
-  
-  hash += (hash << 3);
-  hash ^= (hash >> 11);
-  hash += (hash << 15);
-  return hash;
-}
-
-uint32_t hammingDistance(uint32_t x) {
-
-  uint32_t dist = 0;
-  uint32_t val =   x ^ 0xFFFFFFFF;
-  while(val) {
-    ++dist; 
-    val &= val - 1;
-  }
- 
-  return dist;
 }
