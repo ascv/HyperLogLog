9240451a204b5f660ce32fa4cf37bcb29a422f81
coolSCV <josh.h.andersen@gmail.com>
2012-11-10 20:07:40 -0800

    removed old murmur3 definition

--- loglog.h
+++ loglog.h
@@ -4,12 +4,18 @@
 #include <string.h>
 #include <ctype.h>
 #include <math.h>
+#include "murmur3.h"
 
 uint32_t ones(uint32_t x);
+
 uint32_t lzc(uint32_t x);
+
 uint32_t hamming_distance(uint32_t x);
+
 uint32_t qhashmurmur3_32(const void *data, size_t nbytes);
+
 uint32_t qhashfnv1_32(const void *data, size_t nbytes);
+
 uint32_t jenkins_one_at_a_time_hash(const void * data, size_t len);
 
 
