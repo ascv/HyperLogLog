53797f6490be63218f8d0cbefe103d12fa1c0cdd
coolSCV <josh.h.andersen@gmail.com>
2012-11-11 06:11:03 -0800

    container for helper functions

---
+++ lib.c
@@ -0,0 +1,38 @@
+#include "loglog.h"
+
+/*
+* Get the hamming distance of x and 0xFFFFFFFF.
+*/
+uint32_t hammingDistance(uint32_t x) {
+  uint32_t dist = 0;
+  uint32_t val =   x ^ 0xFFFFFFFF;
+  while(val) {
+    ++dist; 
+    val &= val - 1;
+  }
+  return dist;
+}
+
+/* 
+* Get the number of leading zeros in x.
+*/
+uint32_t leadingZeroCount(uint32_t x) {
+  x |= (x >> 1);
+  x |= (x >> 2);
+  x |= (x >> 4);
+  x |= (x >> 8);
+  x |= (x >> 16);
+  return (32 - ones(x));
+}
+
+/*
+* Get the number of bits set to 1 in x.
+*/
+uint32_t ones(uint32_t x) {
+  x -= (x >> 1) & 0x55555555;
+  x = ((x >> 2) & 0x33333333) + (x & 0x33333333);
+  x = ((x >> 4) + x) & 0x0F0F0F0F;
+  x += (x >> 8);
+  x += (x >> 16);
+  return(x & 0x0000003F);
+}
