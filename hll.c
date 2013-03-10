e3ddee4dd1fdf9b58594f88aa548f7f3bdaf7590
coolSCV <josh.h.andersen@gmail.com>
2013-03-09 22:56:51 -0800

    fixed indices bug

--- hll.c
+++ hll.c
@@ -9,7 +9,7 @@
 typedef struct {
     PyObject_HEAD
     short int k;
-    uint32_t size;
+    uint32_t size; /* number of registers = 2^k */
     char * registers;
 
 } HyperLogLog;
@@ -21,11 +21,19 @@ HyperLogLog_dealloc(HyperLogLog* self)
 }
 
 static PyObject *
-HyperLogLog_new(PyTypeObject *type, PyObject *args, PyObject *kwds)
+HyperLogLog_new(PyTypeObject *type, PyObject *args)
 {
+  /*
+  static char *kwlist[] = {"power", "seed", NULL}; // TODO: add seed keyword
+  */
     HyperLogLog *self;
     self = (HyperLogLog *)type->tp_alloc(type, 0);
-
+    /*
+    if (! PyArg_ParseTupleAndKeywords(args, kwds, "|OOi", kwlist, 
+                                      &first, &last, 
+                                      &self->number))
+        return -1; 
+    */
     if (!PyArg_ParseTuple(args, "i", &self->k)) {
         return NULL;
     }
@@ -53,25 +61,24 @@ static PyMemberDef HyperLogLog_members[] = {
 };
 
 static PyObject *
-HyperLogLog_add(HyperLogLog *self, PyObject * args) // TODO: add seed argument
+HyperLogLog_add(HyperLogLog *self, PyObject *args)
 {
     const char *data;
     const uint32_t dataLength;
 
     if (!PyArg_ParseTuple(args, "s#", &data, &dataLength))
-        return NULL; //TODO: add error
+        return NULL;
 
     uint32_t *hash = (uint32_t *) malloc(sizeof(uint32_t));
     uint32_t index;
     uint32_t rank;
 
-    //TODO: change 42 to random seed
-    MurmurHash3_x86_32((void *) data, dataLength, 4294, (void *) hash);
+    MurmurHash3_x86_32((void *) data, dataLength, 314, (void *) hash);
 
-    // get the first k bits as an int
-    index = *hash >> (32 - self->k);
+    /* use the first k bits + 1 as in index*/
+    index = (*hash >> (32 - self->k)) + 1;
 
-    // get the rank of the remaining 32-k bits
+    /* get the rank of the remaining 32-k bits */
     rank = leadingZeroCount((*hash << self->k) >> self->k) - self->k + 1;
     
     if (rank > self->registers[index])
@@ -84,6 +91,9 @@ HyperLogLog_add(HyperLogLog *self, PyObject * args) // TODO: add seed argument
 static PyObject *
 HyperLogLog_cardinality(HyperLogLog *self)
 {
+    static const double two_32 = 4294967296.0;
+    static const double neg_two_32 = -4294967296.0;
+
     double alpha = 0.0;
     switch (self->size) {
         case 16:
@@ -108,10 +118,7 @@ HyperLogLog_cardinality(HyperLogLog *self)
         sum = sum + 1.0/pow(2, rank);
     }
 
-    static const double two_32 = 4294967296.0;
-    static const double neg_two_32 = -4294967296.0;
-    double estimate = alpha * (1/sum) * self->size * self->size;
-    
+    double estimate = alpha * (1/sum) * self->size * self->size;    
     if (estimate <= 2.5 * self->size) {
         uint32_t zeros = 0;
 	uint32_t i;
@@ -127,13 +134,23 @@ HyperLogLog_cardinality(HyperLogLog *self)
     
     if (estimate <= (1.0/30.0) * two_32)
         estimate = estimate;
-
     else if (estimate > (1.0/30.0) * two_32)
         estimate = neg_two_32 * log(1.0 - estimate/two_32);
 
     return Py_BuildValue("d", estimate);
 }
+/*
+static PyObject *
+HyperLogLog_merge(HyperLogLog *self, PyObject * args) 
+{
+    uint32_t i;
+    // verify size of other HLL works with current size
+    // get registers of other hyperloglog
+    // loop across current registers, comparing and setting
 
+    return Py_BuildValue("s#", self->registers, self->size);
+} 
+*/
 static PyObject *
 HyperLogLog_registers(HyperLogLog *self)
 {
@@ -146,14 +163,16 @@ HyperLogLog_size(HyperLogLog* self)
     return Py_BuildValue("i", self->size);
 }
 
-
 static PyMethodDef HyperLogLog_methods[] = {
     {"add", (PyCFunction)HyperLogLog_add, METH_VARARGS,
      "Add an element to a random register."
     },
     {"cardinality", (PyCFunction)HyperLogLog_cardinality, METH_NOARGS,
      "Get the cardinality."
-    },
+    },/*
+    {"merge", (PyCFunction)HyperLogLog_merge, METH_VARARGS,
+     "Merge another HyperLogLog object with the current object."
+     },*/
     {"registers", (PyCFunction)HyperLogLog_registers, METH_NOARGS, 
      "Get a string copy of the registers."
      },
@@ -167,33 +186,33 @@ static PyTypeObject HyperLogLogType = {
     PyObject_HEAD_INIT(NULL)
     0,                         /*ob_size*/
     "HLL.HyperLogLog",         /*tp_name*/
-    sizeof(HyperLogLog),             /*tp_basicsize*/
+    sizeof(HyperLogLog),       /*tp_basicsize*/
     0,                         /*tp_itemsize*/
     (destructor)HyperLogLog_dealloc, /*tp_dealloc*/
     0,                         /*tp_print*/
     0,                         /*tp_getattr*/
     0,                         /*tp_setattr*/ 
-    0,                         /*tp_compare*/ // compare cardinality
+    0,                         /*tp_compare*/
     0,                         /*tp_repr*/
-    0,                         /*tp_as_number*/ // return cardinality
+    0,                         /*tp_as_number*/
     0,                         /*tp_as_sequence*/
     0,                         /*tp_as_mapping*/
     0,                         /*tp_hash */
     0,                         /*tp_call*/
-    0,                         /*tp_str*/ // return registers
+    0,                         /*tp_str*/
     0,                         /*tp_getattro*/
     0,                         /*tp_setattro*/
     0,                         /*tp_as_buffer*/
     Py_TPFLAGS_DEFAULT | Py_TPFLAGS_BASETYPE, /*tp_flags*/
-    "HyperLogLog object",           /* tp_doc */
+    "HyperLogLog object",      /* tp_doc */
     0,		               /* tp_traverse */
     0,		               /* tp_clear */
     0,		               /* tp_richcompare */
     0,		               /* tp_weaklistoffset */
     0,		               /* tp_iter */
     0,		               /* tp_iternext */
-    HyperLogLog_methods,             /* tp_methods */
-    HyperLogLog_members,             /* tp_members */
+    HyperLogLog_methods,       /* tp_methods */
+    HyperLogLog_members,       /* tp_members */
     0,                         /* tp_getset */
     0,                         /* tp_base */
     0,                         /* tp_dict */
@@ -202,7 +221,7 @@ static PyTypeObject HyperLogLogType = {
     0,                         /* tp_dictoffset */
     (initproc)HyperLogLog_init,      /* tp_init */
     0,                         /* tp_alloc */
-    HyperLogLog_new,                 /* tp_new */
+    HyperLogLog_new,           /* tp_new */
 };
 
 static PyMethodDef module_methods[] = {
@@ -229,7 +248,7 @@ PyMODINIT_FUNC initHLL(void)
     PyModule_AddObject(m, "HyperLogLog", (PyObject *)&HyperLogLogType);
 }
 
-// TODO: move these to another file
+
 
 /* 
  * Get the number of leading zeros in |x|.
