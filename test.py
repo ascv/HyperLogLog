e1e4a4202a531496cb6ae77e580276d72bfa09f4
ascv <josh.h.andersen@gmail.com>
2013-07-28 04:46:00 -0700

    fixed mixed tabs and whitespace

--- test.py
+++ test.py
@@ -4,39 +4,39 @@ import unittest
 class TestRegisterFunctions(unittest.TestCase):
 
     def setUp(self):
-	self.k = 5
-	self.hll = HyperLogLog(5)
+        self.k = 5
+        self.hll = HyperLogLog(5)
 
     def test_set_last_register(self):
-	self.hll.set_register(self.k - 1, 1)
-	self.assertTrue(self.hll.registers()[self.k - 1] == 1)
+        self.hll.set_register(self.k - 1, 1)
+        self.assertTrue(self.hll.registers()[self.k - 1] == 1)
 
     def test_set_first_register(self):
-	self.hll.set_register(0, 1)
-	self.assertTrue(self.hll.registers()[0] == 1)
+        self.hll.set_register(0, 1)
+        self.assertTrue(self.hll.registers()[0] == 1)
 
     def test_registers_returns_bytesarray(self):
-	self.assertTrue(type(self.hll.registers()) is bytearray)
+        self.assertTrue(type(self.hll.registers()) is bytearray)
 
     def test_registesr_returns_correct_length_bytes_array(self):
-	self.assertTrue(len(self.hll.registers()) == pow(2, self.k))
+        self.assertTrue(len(self.hll.registers()) == pow(2, self.k))
 
 class TestCardinalityEstimation(unittest.TestCase):
 
     def setUp(self):
-	""" """
+        """ """
 
     def test_add_adds_an_element_to_a_random_register(self):
         """ """
 
     def test_small_range_correction(self):
-	""" """
+        """ """
 
     def test_medium_range_no_correction(self):
-	""" """
+        """ """
 
     def test_large_range_correction(self):
- 	""" """
+        """ """
 
     def test_the_larger_rank_is_used_when_comparing_elements(self):
         """ """
@@ -44,14 +44,13 @@ class TestCardinalityEstimation(unittest.TestCase):
 class TestHyperLogLogConstructor(unittest.TestCase):
 
     def setUp(self):
-	""" """
+        """ """
 
     def test_non_power_of_2_size_is_invalid(self):
-	""" """
-
+        """ """
 
     def test_negative_size_is_invalid(self):
-    	""" """
+        """ """
 
     def test_size_2_is_valid(self):
         """ """
@@ -68,7 +67,7 @@ class TestHyperLogLogConstructor(unittest.TestCase):
 class TestMerging(unittest.TestCase):
 
     def setUp(self):
-	""" """
+        """ """
 
     def test_only_same_size_HyperLogLogs_can_be_merged(self):
         """ """
@@ -76,18 +75,8 @@ class TestMerging(unittest.TestCase):
     def test_merge(self):
         """ """
 
-
-
 if __name__ == '__main__':
     unittest.main()
-
-
-
-
-
-
-
-
 
 """
 import HLL
@@ -110,12 +99,8 @@ n.set_register(200,0)
 n.set_register(300,0)
 n.set_register(300,0)
 
-
-
 print n.cardinality()
 
-
-#YT343axlkepskkde0
 # intersection(hyperloglog)
 # hash(data)
 # fold()
