c31cbbf89032d00e8944c8aa48bd0ac00c5e726b
ascv <josh.h.andersen@gmail.com>
2013-09-01 16:49:12 -0700

    incremented version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup, Extension
 
 setup(
     name="HLL", 
-    version="0.6.1", 
+    version="0.7", 
     description='HyperLogLog algorithm written in C',
     author="Joshua Andersen",
     url='https://github.com/ascv/HLL',
