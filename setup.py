020ce083442b547980e0840e45979f4e41c39bd5
Josh <josh@classifiedads.com>
2014-01-17 11:39:00 -0800

    added headers

--- setup.py
+++ setup.py
@@ -3,10 +3,11 @@ from distutils.core import setup, Extension
 setup(
     name="HLL", 
     version="0.8", 
-    description='HyperLogLog algorithm written in C',
+    description='The Hyper LogLog algorithm written in C.',
     author="Joshua Andersen",
     url='https://github.com/ascv/HLL',
     ext_modules=[
         Extension("HLL", ["hll.c", "murmur3.c"]),
-    ]
+    ],
+    headers=['hll.h','mumur3.h']
 )
