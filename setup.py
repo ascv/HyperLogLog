b6beac6a2232d83cbf7269619f956ddc53bf09bd
Josh <josh@classifiedads.com>
2014-01-17 11:51:49 -0800

    fixed mispelling of header file

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from distutils.core import setup, Extension
 
 setup(
     name="HLL", 
-    version="0.83", 
+    version="0.831", 
     description='The Hyper LogLog algorithm written in C.',
     author="Joshua Andersen",
     url='https://github.com/ascv/HLL',
     ext_modules=[
         Extension("HLL", ["hll.c", "murmur3.c"]),
     ],
-    headers=['hll.h','mumur3.h']
+    headers=['hll.h','murmur3.h']
 )
