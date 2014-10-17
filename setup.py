3ea03a18c1d0cd55f9b0966b6f8a40f05a1d755b
Josh Andersen <josh@classifiedads.com>
2014-10-16 22:02:48 -0700

    bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup, Extension
 
 setup(
     name='HLL',
-    version='0.84',
+    version='0.9',
     description='HyperLogLog implementation in C.',
     author='Joshua Andersen',
     author_email='anderj0@uw.edu',
