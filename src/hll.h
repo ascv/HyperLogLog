eaeae3e6c457e04daa3e098644134f6251d7ee42
Josh Andersen <josh@classifiedads.com>
2018-05-20 17:41:52 -0700

    Update cardinality estimation algorithm
    The new algorithm [1] introduces some notable improvements:
    
    - Support for much larger cardinalities. This is result of using a 64
      bit hash. As a consequence, large range correction is no longer
      necessary.
    - Improved accuracy at small cardinalities. Doing a linear count (small
      range correction) is no longer necessary. This fixes the spike in
      relative error when the original algorithm switched from a linear
      count [3] to a hyperloglog estimate.
    - Improved accuracy at large cardinalities without the need for large
      range correction. This is an improvement over variants HLL++, and Sliding
      HyperLogLog [2] which used empirically derived weights
    
    Implementation is accompanied by the following changes:
    
    - Switch from the 32 bit MurmurHash3 to the 64 bit MurmurHash64A. Using a
      64 bit hash significantly increases the maximum cardinalities that can be
      estimated.
    - A faster algorithm for computing the number of leading zeros. The new
      algorithm determines the byte the first set bit occurs in and then
      uses a lookup table to get the number of leading zeroes. This
      guarantees the
    - Fix segfault caused by attempting to allocate too much memory. If there is
      insufficient memory for the registers then an Exception is thrown.
    
    [1] "New cardinality estimation algorithms for HyperLogLog Sketches"
    Omar Ertl 2017 arvix

--- src/hll.h
+++ src/hll.h
@@ -1,5 +1,5 @@
 #include <stdint.h>
 
-uint32_t leadingZeroCount(uint32_t x);
-
-uint32_t ones(uint32_t x);
+static inline uint8_t clz(uint64_t x);
+static inline double sigma(double x);
+static inline double tau(double x);
