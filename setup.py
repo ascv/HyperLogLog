6eea9aa1120b165762ddb1227e62d9a66a352840
Josh Andersen <josh@classifiedads.com>
2018-05-20 17:41:52 -0700

    Update cardinality estimation algorithm
    The new algorithm [1] introduces some notable improvements:
    
    - Support for much larger cardinalities. This is result of using a 64
      bit hash. As a consequence, large range correction is no longer
      necessary.
    - Improved accuracy at small cardinalities. Doing a linear count (small
      range correction) is no longer necessary. This fixes the spike in
      relative error when the original algorithm switched from a linear
      count [3] to a hyperloglog estimate.
    - Improved accuracy at large cardinalities without the need for large
      range correction. This is an improvement over variants HLL++, and Sliding
      HyperLogLog [2] which used empirically derived weights
    
    Implementation is accompanied by the following changes:
    
    - Switch from the 32 bit MurmurHash3 to the 64 bit MurmurHash64A. Using a
      64 bit hash significantly increases the maximum cardinalities that can be
      estimated.
    - A faster algorithm for computing the number of leading zeros. The new
      algorithm determines the byte the first set bit occurs in and then
      uses a lookup table to get the number of leading zeroes. This
      guarantees the
    - Fix segfault caused by attempting to allocate too much memory. If there is
      insufficient memory for the registers then an Exception is thrown.
    
    [1] "New cardinality estimation algorithms for HyperLogLog Sketches"
    Omar Ertl 2017 arvix

--- setup.py
+++ setup.py
@@ -2,23 +2,25 @@ from distutils.core import setup, Extension
 
 setup(
     name='HLL',
-    version='1.2.8',
+    version='2.0.0',
     description='HyperLogLog implementation in C for python.',
     author='Joshua Andersen',
-    author_email='anderj0@uw.edu',
+    author_email='josh.h.andersen@gmail.com',
     maintainer='Joshua Andersen',
     url='https://github.com/ascv/HyperLogLog',
     ext_modules=[
-        Extension('HLL', ['src/hll.c', 'lib/murmur3.c']),
+        Extension('HLL', ['src/hll.c', 'lib/murmur3.c', 'lib/murmur2.c']),
     ],
-    headers=['src/hll.h', 'lib/murmur3.h'],
-    keywords=['HyperLogLog', 'Hyper LogLog', 'LogLog', 'cardinality', 'counting', 'sketch'],
+    headers=['src/hll.h', 'lib/murmur3.h', 'lib/murmur2.h'],
+    keywords=['HyperLogLog', 'Hyper LogLog', 'Hyper Log Log', 'LogLog', 'Log Log', 'cardinality', 'counting', 'sketch'],
     license='MIT',
     long_description=\
 """
 The HyperLogLog algorithm [1] is a space efficient method to estimate the
 cardinality of extraordinarily large data sets. This module is written in C
-using a Murmur3 hash, for python 2.7.x or python 3.x.
+and uses the Murmurhash64A hash function. It uses an improved version of the
+algorithm [2] that significantly larger cardinalities with better accuracy.
+
 
 [1] http://algo.inria.fr/flajolet/Publications/FlFuGaMe07.pdf
 """
