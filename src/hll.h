1336e5266331d2039bf59740d8c92fc57931f81d
Josh Andersen <josh@classifiedads.com>
2019-11-29 23:36:38 -0800

    Use dense register encoding
    This encodes register values using only 6 bits. Previously, an entire
    byte was used for each register which is wasteful since register values
    will never exceed 63.

--- src/hll.h
+++ src/hll.h
@@ -3,3 +3,9 @@
 static inline uint8_t clz(uint64_t x);
 static inline double sigma(double x);
 static inline double tau(double x);
+
+static inline void setReg(uint64_t m, uint8_t n, char *regs);
+static inline uint64_t getReg(uint64_t m, char * regs);
+
+void printByte(char a);
+void setMemoryErrorMsg(uint64_t bytes);
