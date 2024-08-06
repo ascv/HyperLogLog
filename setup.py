c26ba9d988c6f13da92f88f62aa7abeba2bab210
Joshua Andersen <josh.h.andersen@gmail.com>
2024-08-05 18:42:20 -0700

    Fix unnecessary package definition

--- setup.py
+++ setup.py
@@ -10,15 +10,13 @@ module = Extension(
 
 setup(
     name='HLL',
-    version='2.1.4',
+    version='2.1.5',
     description='Fast HyperLogLog',
     author='Joshua Andersen',
     author_email='josh.h.andersen@gmail.com',
     maintainer='Joshua Andersen',
     url='https://github.com/ascv/HyperLogLog',
     ext_modules=[module],
-    include_package_data=True,
-    packages=['HLL'],
     zip_safe=False,
     keywords=['algorithm', 'approximate counting', 'big data', 'big data', 'cardinality', 'cardinality estimate', 'counting', 'data analysis', 'data processing', 'data science', 'data sketching', 'efficient computation', 'estimating cardinality', 'fast', 'frequency estimation', 'hyper log log', 'hyper loglog', 'hyperloglog', 'large-scale data', 'log log', 'loglog', 'memory efficient', 'probability estimate', 'probability sketch', 'probablistic counting', 'probablistic data structures', 'real-time analytics', 'scalable', 'set cardinality', 'set operations', 'sketch', 'statistical analysis', 'streaming algorithms', 'streaming algorithms', 'unique count', 'unique element counting'],
     license='MIT',
